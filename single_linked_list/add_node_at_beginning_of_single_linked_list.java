c9e580a821912ead99fd0c498a7531e4bf72aa9a
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-23 18:00:26 +0200

    add node at the beginning of single linked list

--- single_linked_list/add_node_at_beginning_of_single_linked_list.java
+++ single_linked_list/add_node_at_beginning_of_single_linked_list.java
@@ -4,12 +4,12 @@ import java.util.LinkedList;
 
 public  class implementation_of_single_linked_list
 {
-    private org.example.implementation_of_single_linked_list.Node head;
+    private Node head;
 
     private static class Node
     {
         private int data;
-        private org.example.implementation_of_single_linked_list.Node next;
+        private Node next;
 
         public Node(int data)
         {
@@ -22,15 +22,14 @@ public  class implementation_of_single_linked_list
     // which is the most recently added node.
     public void addToFront(int data)
     {
-        org.example.implementation_of_single_linked_list.Node new_node = new org.example.implementation_of_single_linked_list.Node(data);
+        Node new_node = new Node(data);
         new_node.next = head;
         head = new_node;
     }
 
     public void printList()
     {
-        org.example.implementation_of_single_linked_list.Node ptr = head;
-
+        Node ptr = head;
         while (ptr != null)
         {
             System.out.print(ptr.data + " ");
@@ -42,7 +41,7 @@ public  class implementation_of_single_linked_list
 
     public static void main(String[] args)
     {
-        org.example.implementation_of_single_linked_list list = new org.example.implementation_of_single_linked_list();
+        implementation_of_single_linked_list list = new implementation_of_single_linked_list();
 
         list.addToFront(5);
         list.addToFront(10);
